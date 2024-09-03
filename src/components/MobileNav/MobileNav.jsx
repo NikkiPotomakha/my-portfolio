830bcbd0052a1206c49364a71220d558021d49c6
NikkiPotomakha <serhiienkoveronika@gmail.com>
2024-09-03 20:06:02 +0200

    Created Mobile folders for nav, some styles, added icon menu,  deleted and created svg logo.

---
+++ src/components/MobileNav/MobileNav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './MobileNav.css';
+
+const MobileNav = ({ isOpen, toogleMenu }) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MobileNav
