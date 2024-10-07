bfbbe151264a9eb41c76de2ec7a311eb25f49cc8
NikkiPotomakha <serhiienkoveronika@gmail.com>
2024-10-07 17:24:25 +0200

    Added some icons, changed img

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import './Hero.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faFigma } from "@fortawesome/free-brands-svg-icons";
+import { faHtml5 } from "@fortawesome/free-brands-svg-icons";
+import { faCss3 } from "@fortawesome/free-brands-svg-icons";
+import { faReact } from "@fortawesome/free-brands-svg-icons";
+import { faJs } from "@fortawesome/free-brands-svg-icons";
 
 const Hero = () => {
   return (
     <section className="hero-section">
       <div className="hero-container">
-        <div className="hero-title">
-          <h1>HI, I'M VERONIKA</h1>
-        </div>
         <div className="hero-img-container">
-          <img src="/src/images/Frame 2.png" alt="" />
+          <img src="/src/images/Me.png" alt="" />
         </div>
         <div className="hero-content">
-          <h2>A Junior Frontend Developer based in Warsaw, Poland.</h2>
+          <h1>Hi, I'm Veronika</h1>
+          <h2>Frontend Developer based in Warsaw, Poland.</h2>
           <p>
             I have a keen eye for design and a knack for bringing web
             applications to life. With a solid foundation in HTML, CSS, and
@@ -21,25 +26,27 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <div className="icon-title">Tech skills</div>
 
-      <div className="hero-icon">
-        <img src="/src/icons/css3.svg" alt="" />
-      </div>
-      <div className="hero-icon">
-        <img src="/src/icons/figma.svg" alt="" />
-      </div>
-      <div className="hero-icon">
-        <img src="/src/icons/html5.svg" alt="" />
-      </div>
-      <div className="hero-icon">
-        <img src="/src/icons/js.svg" alt="" />
-      </div>
-      <div className="hero-icon">
-        <img src="/src/icons/react.svg" alt="" />
-      </div>
-      <div className="hero-icon">
-        <img src="/src/icons/sql.svg" alt="" />
+      <div className="tech-icon">
+        <div className="icon-title">Tech skills</div>
+        <div className="hero-icon">
+          <FontAwesomeIcon icon={faGithub} />
+        </div>
+        <div className="hero-icon">
+          <FontAwesomeIcon icon={faFigma} />
+        </div>
+        <div className="hero-icon">
+          <FontAwesomeIcon icon={faHtml5} />
+        </div>
+        <div className="hero-icon">
+          <FontAwesomeIcon icon={faCss3} />
+        </div>
+        <div className="hero-icon">
+          <FontAwesomeIcon icon={faJs} />
+        </div>
+        <div className="hero-icon">
+          <FontAwesomeIcon icon={faReact} />
+        </div>
       </div>
     </section>
   );
