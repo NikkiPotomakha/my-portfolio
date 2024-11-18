049583847bc5e0690b1e4367c6ead2c9fb87033a
NikkiPotomakha <serhiienkoveronika@gmail.com>
2024-11-18 19:52:32 +0100

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -10,8 +10,18 @@ const Contact = () => {
           <div className="contact_container container grid">
               <div className="contact-content">
                   <h3 className="conatct-title">
-                      
+                      Talk to me
                   </h3>
+                  <div className="contact-info">
+                      <div className="contact-card">
+                          <i className="contact-card-icon"></i>
+                          <h3 className="contact-card-title"></h3>
+                      </div>
+                  </div>
+              </div>
+
+              <div className="contact-content">
+                  <h3 className="contact_title">Write me your project</h3>
               </div>
           </div>
     </section>
