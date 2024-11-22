04d76e8cd79e6c7185d213baad3d4b2e30ed9bc8
NikkiPotomakha <serhiienkoveronika@gmail.com>
2024-11-22 09:50:14 +0100

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,10 +2,35 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className="footer">
+      <div className="footer_container container">
+        <h1 className="footer_title">Potomakha</h1>
+        <ul className="footer_list">
+          <li>
+            <a href="#about" className="footer_link">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#skills" className="footer_link">
+              Skills
+            </a>
+          </li>
+          <li>
+            <a href="#qualification" className="footer_link">Education</a>
+          </li>
+          <li>
+            <a href="#projects" className="footer_link">
+              Projects
+            </a>
+          </li>
+        </ul>
+        <div className='footer_social'>
+          
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Footer
