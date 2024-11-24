51ca4d9c385f212fa6fb4e90c79456c9bfab5aa2
NikkiPotomakha <serhiienkoveronika@gmail.com>
2024-11-24 21:21:54 +0100

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,71 +1,67 @@
-import React, { useState } from 'react';
-import './components/styles.css/Navbar.css';
-import MobileNav from './components/MobileNav/MobileNav';
+import React, { useState } from "react";
+import "./components/styles.css/Navbar.css";
+import MobileNav from "./components/MobileNav/MobileNav";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
-
-
 const Navbar = () => {
-const [openMenu, setOpenMenu] = useState(false);
+  const [openMenu, setOpenMenu] = useState(false);
 
   const toggleMenu = () => {
-    setOpenMenu(!openMenu);
+    setOpenMenu(!openMenu); // Перемикаємо стан меню
   };
 
-    return (
-      <>
-        <MobileNav isOpen={openMenu} toggleMenu={toggleMenu} />
-
-        <nav className="nav-wrapper">
-          <div className="nav-content">
-            <div className="logo-icon">
-              <a className="logo link" href="/">
-                V<span className="logo link">P</span>
-              </a>
-            </div>
-            <ul>
-              <li>
-                <a href="/" className="menu-item">
-                  HOME
-                </a>
-              </li>
-              <li>
-                <a href="/about" className="menu-item">
-                  ABOUT
-                </a>
-              </li>
-              <li>
-                <a href="#skills" className="menu-item">
-                  SKILLS
-                </a>
-              </li>
-              <li>
-                <a href="#education" className="menu-item">
-                  EDUCATION
-                </a>
-              </li>
-              <li>
-                <a href="#projects" className="menu-item">
-                  PROJECTS
-                </a>
-              </li>
-              <li>
-                <a href="#contact" className="menu-item">
-                  CONTACT
-                </a>
-              </li>
-              <button className="contact-btn" onClick={() => {}}>
-                Hire me
-              </button>
-            </ul>
-            <button className="menu-btn" onClick={toggleMenu}>
-              <FontAwesomeIcon icon={openMenu ? faXmark : faBars} />
-            </button>
+  return (
+    <>
+      <MobileNav isOpen={openMenu} toggleMenu={toggleMenu} />{" "}
+      {/* Передаємо пропси */}
+      <nav className="nav-wrapper">
+        <div className="nav-content">
+          <div className="logo-icon">
+            <a className="logo link" href="/">
+              V<span className="logo link">P</span>
+            </a>
           </div>
-        </nav>
-      </>
-    );
+          <ul>
+            <li>
+              <a href="#home" className="menu-item">
+                HOME
+              </a>
+            </li>
+            <li>
+              <a href="#about" className="menu-item">
+                ABOUT
+              </a>
+            </li>
+            <li>
+              <a href="#skills" className="menu-item">
+                SKILLS
+              </a>
+            </li>
+            <li>
+              <a href="#education" className="menu-item">
+                EDUCATION
+              </a>
+            </li>
+            <li>
+              <a href="#projects" className="menu-item">
+                PROJECTS
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="menu-item">
+                CONTACT
+              </a>
+            </li>
+          </ul>
+          <button className="menu-btn" onClick={toggleMenu}>
+            <FontAwesomeIcon icon={openMenu ? faXmark : faBars} />{" "}
+            {/* Змінюємо іконку на X, коли меню відкрите */}
+          </button>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
