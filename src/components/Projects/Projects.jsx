01c3672c7a0291b5beaef3db526938418bd1a9e9
NikkiPotomakha <serhiienkoveronika@gmail.com>
2024-11-25 08:42:22 +0100

    Update Projects.jsx

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
-import "./Projects.css"
-import mywork_data from '../../project_data';
+import React from "react";
+import "./Projects.css";
+import mywork_data from "../../project_data";
 
 const Projects = () => {
-    return (
-      <section className="projects section" id="projects">
-        <div className="mywork container">
-          <div className="mywork_title">
-            <h1>My Latest Projects</h1>
-          </div>
-          <div className="mywork_container">
-            {mywork_data.map((work, index) => {
-              return <img key={index} src={work.w_img} alt="" />;
-            })}
-          </div>
+  return (
+    <section className="projects section" id="projects">
+      <div className="mywork container">
+        <div className="mywork_title">
+          <h1>My Latest Projects</h1>
         </div>
-      </section>
-    );
-}
+        <div className="mywork_container">
+          {mywork_data.map((work, index) => {
+            return (
+              <a
+                key={index}
+                href={work.w_link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={work.w_img} alt="" />
+              </a>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Projects
+export default Projects;
