436b17b33e5a808eeaf9303e9cdaa63d953991e1
NikkiPotomakha <serhiienkoveronika@gmail.com>
2024-11-25 09:24:07 +0100

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import Book from "../../images/Book.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -7,29 +7,31 @@ import {
   faCss3,
   faReact,
   faJs,
-  faFigma
+  faFigma,
 } from "@fortawesome/free-brands-svg-icons";
-
 
 const About = () => {
   return (
-    <div className="about_section" id='about'>
+    <div className="about_section" id="about">
       <div className="about_container">
         <h3 className="about_title">About Me</h3>
-        <h4 className="about_subtitle">Let's diving into my world</h4>
+        <h4 className="about_subtitle">Let's dive into my world</h4>
         <p className="about_text">
-          Learn more about who I am, what I do and what inspires me
+          Learn more about who I am, what I do, and what inspires me.
         </p>
       </div>
-      <div>
+
+      <div className="card_section">
+        {/* Current Read Card */}
         <div className="card current_read">
-          <h4>My reads</h4>
+          <h4>My Reads</h4>
           <p>Taming Tigers - Jim Lawless</p>
           <div className="book_image">
             <img src={Book} alt="Taming Tigers" />
           </div>
         </div>
-        {/* My Tech Stacks Section */}
+
+        {/* Tech Stack Card */}
         <div className="card tech_stack">
           <h4>My Tech Stacks</h4>
           <p>
@@ -37,36 +39,28 @@ const About = () => {
           </p>
           <div className="tech_stack_icons">
             <FontAwesomeIcon icon={faGithub} />
-          </div>
-          <div className="tech_stack_icons">
             <FontAwesomeIcon icon={faFigma} />
-          </div>
-          <div className="tech_stack_icons">
             <FontAwesomeIcon icon={faHtml5} />
-          </div>
-          <div className="tech_stack_icons">
             <FontAwesomeIcon icon={faCss3} />
-          </div>
-          <div className="tech_stack_icons">
             <FontAwesomeIcon icon={faJs} />
-          </div>
-          <div className="hero-icon">
             <FontAwesomeIcon icon={faReact} />
           </div>
         </div>
 
-        {/* My Persona Section */}
+        {/* Persona Card */}
         <div className="card persona">
           <h4>My Persona</h4>
           <p>Know me as a person</p>
           <div className="persona_tags">
             <span>Traveller ✈️</span>
             <span>Social-animal 😍</span>
+            <span>Photographer 📸</span>
+            <span>Fitness Enthusiast 💪</span>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default About;
